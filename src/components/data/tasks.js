84da78679d11dfc87278a44c0449f75d9dcbc5d4
DavidMenjivar486 <davidmenjivarr8000@gmail.com>
2023-08-08 10:13:00 -0600

    Complete project

---
+++ src/components/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title: "Tarea1",
+        description: "Mi primer tarea"
+    },
+    {
+        id:1,
+        title: "Tarea2",
+        description: "Mi segunda tarea"
+    },
+    {
+        id:2,
+        title: "Tarea3",
+        description: "Mi tercer tarea"
+    },
+]
